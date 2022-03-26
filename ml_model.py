3cfc9ca4f9c82053584a18da2aa5156c03d66640
RashmiJennefer <jennefer2010@gmail.com>
2022-03-26 18:09:38 +0530

    added 3 new ML Model code

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,11 @@
 
 print("Adding all ml related code here")
 
+def model1():
+    print("adding code for Ml Model 1")
+
+def model2():
+    print("adding code for Ml Model 2")
+
+def model3():
+    print("adding code for Ml Model 3")
